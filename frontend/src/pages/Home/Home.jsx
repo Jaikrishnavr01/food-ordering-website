5ecec15971acafcedd67320fc187a9b8591560d9
Jaikrishnavr01 <jaikrishnavr@outlook.com>
2024-03-31 08:44:09 +0530

    create food list components

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import SiteUnderContruction from '../../components/SiteUnderContrucrion/SiteUnderContruction'
 
 const Home = () => {
 
@@ -10,6 +12,8 @@ const Home = () => {
     <div>
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
+      <SiteUnderContruction/>
     </div>
   )
 }
