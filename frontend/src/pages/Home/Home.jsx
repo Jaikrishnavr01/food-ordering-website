b7c412e5ae431de0aeb55ebb63dd5457d2bfbcc3
jaikrishnavr01 <jaikrishnavr@outlook.com>
2024-04-16 15:59:41 +0530

    footer & App Download component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import SiteUnderContruction from '../../components/SiteUnderContrucrion/SiteUnderContruction'
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 const Home = () => {
 
@@ -13,7 +14,8 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
-      <SiteUnderContruction/>
+      <AppDownload/>
+      {/* <SiteUnderContruction/> */}
     </div>
   )
 }
