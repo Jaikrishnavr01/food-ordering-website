0dcf096f04785f1c08d92080706c8019846fe755
Jaikrishnavr01 <jaikrishnavr@outlook.com>
2024-04-21 12:49:21 +0530

    carts page incomptete

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,17 +9,19 @@ const Navbar = ({setShowLogin}) => {
 
     return (
         <div className='navbar'>
+            <Link to='/'>
             <img src={assets.logo1} alt="logo" className='logo' />
+            </Link>
             <ul className="navbar-menu">
                 <Link to='/' onClick={()=> setMenu("home")} className={menu === "home" ? "active" : ""}>home</Link>
-                <a href='#explore-menu' onClick={()=> setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
+                <Link to='/'> </Link>  <a href='#explore-menu' onClick={()=> setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
                 <a href='#app-download' onClick={()=> setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</a>
                 <a href='#footer' onClick={()=> setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>contact-us</a>
             </ul>
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="search-icon" className='navbar-search-icon' />
                 <div className="navbar-search-icon">
-                    <img src={assets.basket_icon} alt="basket-icon" />
+                    <Link to='/cart'><img src={assets.basket_icon} alt="basket-icon" /></Link>
                     <div className="dot"></div>
                 </div>
                 <button onClick={()=> setShowLogin(true)}>sign in</button>
