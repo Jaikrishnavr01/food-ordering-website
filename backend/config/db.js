fd1aa6216e63aa438d3bc845968212cb48be3eb0
Jaikrishnavr01 <jaikrishnavr@outlook.com>
2024-06-01 12:26:18 +0530

    db changed to atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDb = async () => {
-    await mongoose.connect("mongodb://127.0.0.1:27017/fooddelivery").then(()=>console.log("Db connected"))
+    await mongoose.connect("mongodb+srv://jaikrishnagokul:AMbbaRnALuInsNe4@cluster0.40b06u8.mongodb.net/").then(()=>console.log("Db connected"))
 }
